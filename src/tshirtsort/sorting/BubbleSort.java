38b14993484250295c785d531965ea6bb329836e
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-16 19:36:36 +0300

    Adjust bubble sort to sorting strategy approach

--- src/tshirtsort/sorting/BubbleSort.java
+++ src/tshirtsort/sorting/BubbleSort.java
@@ -1,91 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tshirtsort.sorting;
 
 import java.util.List;
 import tshirtsort.models.TShirt;
 
-/**
- *
- * @author mac
- */
 public class BubbleSort {
 
-    // boolean sortType, sortType == true, ASC --  sortType == false, DESC
-    // int sortByProperty 
-    // sortByProperty == 1 -- Size
-    // sortByProperty == 2 -- Color 
-    // sortByProperty == 3 -- Fabric 
-    void bubbleSort(List<TShirt> arr, boolean sortType, int sortByProperty) {
+    public void sort(List<TShirt> arr, ISortingStrategy sortingStrategy) {
         int n = arr.size();
+        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
-                switch (sortByProperty) {
-                    // Size - 1
-                    case 1:
-                        // ASC
-                        if(sortType) {
-                            if (arr.get(j).getSize().ordinal() > arr.get(j + 1).getSize().ordinal()) {
-                                // swap arr[j+1] and arr[i] 
-                                TShirt temp = arr.get(j);
-                                arr.set(j, arr.get(j + 1));
-                                arr.set(j + 1, temp);
-                            }
-                        } else { // DESC
-                            if (arr.get(j).getSize().ordinal() < arr.get(j + 1).getSize().ordinal()) {
-                                // swap arr[j+1] and arr[i] 
-                                TShirt temp = arr.get(j);
-                                arr.set(j, arr.get(j + 1));
-                                arr.set(j + 1, temp);
-                            }
-                        }
-                        
-                        break;
-                    // Color - 2    
-                    case 2:
-                        // ASC
-                        if(sortType) {
-                            if (arr.get(j).getColor().ordinal() > arr.get(j + 1).getColor().ordinal()) {
-                                // swap arr[j+1] and arr[i] 
-                                TShirt temp = arr.get(j);
-                                arr.set(j, arr.get(j + 1));
-                                arr.set(j + 1, temp);
-                            }
-                        } else { // DESC
-                            if (arr.get(j).getColor().ordinal() < arr.get(j + 1).getColor().ordinal()) {
-                                // swap arr[j+1] and arr[i] 
-                                TShirt temp = arr.get(j);
-                                arr.set(j, arr.get(j + 1));
-                                arr.set(j + 1, temp);
-                            }
-                        }
-                        break;
-                    // Fabric - 3
-                    case 3:
-                        // ASC
-                        if(sortType) {
-                            if (arr.get(j).getFabric().ordinal() > arr.get(j + 1).getFabric().ordinal()) {
-                                // swap arr[j+1] and arr[i] 
-                                TShirt temp = arr.get(j);
-                                arr.set(j, arr.get(j + 1));
-                                arr.set(j + 1, temp);
-                            }
-                        } else { // DESC
-                            if (arr.get(j).getFabric().ordinal() < arr.get(j + 1).getFabric().ordinal()) {
-                                // swap arr[j+1] and arr[i] 
-                                TShirt temp = arr.get(j);
-                                arr.set(j, arr.get(j + 1));
-                                arr.set(j + 1, temp);
-                            }
-                        }
-                        break;
+                int compareResult = sortingStrategy.compare(arr.get(j), arr.get(j + 1));
+                if (compareResult < 0) {
+
+                    TShirt temp = arr.get(j);
+                    arr.set(j, arr.get(j + 1));
+                    arr.set(j + 1, temp);
+
+                    swapped = true;
                 }
             }
+            if (swapped == false) {
+                break;
+            }
         }
-
     }
-
 }
