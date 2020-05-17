266e8632fc3f937e61bd0cfb1626493dfa11f036
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-17 23:06:01 +0300

    Add unit tests for other sorting algorithms

---
+++ test/SortingAlgorithmsTest/QuickSortTest.java
@@ -0,0 +1,127 @@
+package SortingAlgorithmsTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tshirtsort.models.Color;
+import tshirtsort.models.Fabric;
+import tshirtsort.models.Size;
+import tshirtsort.models.TShirt;
+import tshirtsort.sorting.algorithms.ISortingAlgorithm;
+import tshirtsort.sorting.algorithms.QuickSort;
+import tshirtsort.sortingstrategies.ISortingStrategy;
+import tshirtsort.sortingstrategies.SortByColorAsc;
+import tshirtsort.sortingstrategies.SortByColorDesc;
+import tshirtsort.sortingstrategies.SortByFabricAsc;
+import tshirtsort.sortingstrategies.SortByFabricDesc;
+import tshirtsort.sortingstrategies.SortBySizeAsc;
+import tshirtsort.sortingstrategies.SortBySizeDesc;
+
+public class QuickSortTest {
+
+    ISortingAlgorithm quickSort = new QuickSort();
+    List<TShirt> tShirts = new ArrayList();
+
+    ISortingStrategy byColorAsc = new SortByColorAsc();
+    ISortingStrategy byColorDesc = new SortByColorDesc();
+    ISortingStrategy bySizeAsc = new SortBySizeAsc();
+    ISortingStrategy bySizeDesc = new SortBySizeDesc();
+    ISortingStrategy byFabricAsc = new SortByFabricAsc();
+    ISortingStrategy byFabricDesc = new SortByFabricDesc();
+
+    TShirt tShirt1 = new TShirt("Red-S-Cotton", Color.RED, Size.S, Fabric.COTTON);
+    TShirt tShirt2 = new TShirt("Blue-M-Silk", Color.BLUE, Size.M, Fabric.SILK);
+    TShirt tShirt3 = new TShirt("Violette-XXL-Polyester", Color.VIOLET, Size.XXL, Fabric.POLYESTER);
+    TShirt tShirt4 = new TShirt("Indigo-XS-Cashmere", Color.INDIGO, Size.XS, Fabric.CASHMERE);
+    TShirt tShirt5 = new TShirt("Green-XXXL-Wool", Color.GREEN, Size.XXXL, Fabric.WOOL);
+
+    public QuickSortTest() {
+    }
+
+    @Before
+    public void setUp() {
+        tShirts.add(tShirt1);
+        tShirts.add(tShirt2);
+        tShirts.add(tShirt3);
+        tShirts.add(tShirt4);
+        tShirts.add(tShirt5);
+    }
+
+    @Test
+    public void QuickSortTestByColorAscTest() {
+        quickSort.sort(tShirts, byColorAsc);
+        List<TShirt> sortedTShirts = new ArrayList<>();
+        sortedTShirts.add(tShirt1);
+        sortedTShirts.add(tShirt5);
+        sortedTShirts.add(tShirt2);
+        sortedTShirts.add(tShirt4);
+        sortedTShirts.add(tShirt3);
+        Assert.assertArrayEquals(sortedTShirts.toArray(), tShirts.toArray());
+    }
+
+    @Test
+    public void QuickSortTestByColorDescTest() {
+        quickSort.sort(tShirts, byColorDesc);
+        List<TShirt> sortedTShirts = new ArrayList<>();
+        sortedTShirts.add(tShirt3);
+        sortedTShirts.add(tShirt4);
+        sortedTShirts.add(tShirt2);
+        sortedTShirts.add(tShirt5);
+        sortedTShirts.add(tShirt1);
+        Assert.assertArrayEquals(sortedTShirts.toArray(), tShirts.toArray());
+    }
+
+    @Test
+    public void QuickSortTestBySizeAscTest() {
+        quickSort.sort(tShirts, bySizeAsc);
+        List<TShirt> sortedTShirts = new ArrayList<>();
+        sortedTShirts.add(tShirt4);
+        sortedTShirts.add(tShirt1);
+        sortedTShirts.add(tShirt2);
+        sortedTShirts.add(tShirt3);
+        sortedTShirts.add(tShirt5);
+        Assert.assertArrayEquals(sortedTShirts.toArray(), tShirts.toArray());
+
+    }
+
+    @Test
+    public void QuickSortTestBySizeDescTest() {
+        quickSort.sort(tShirts, bySizeDesc);
+        List<TShirt> sortedTShirts = new ArrayList<>();
+        sortedTShirts.add(tShirt5);
+        sortedTShirts.add(tShirt3);
+        sortedTShirts.add(tShirt2);
+        sortedTShirts.add(tShirt1);
+        sortedTShirts.add(tShirt4);
+        Assert.assertArrayEquals(sortedTShirts.toArray(), tShirts.toArray());
+
+    }
+
+    @Test
+    public void QuickSortTestByFabricAscTest() {
+        quickSort.sort(tShirts, byFabricAsc);
+        List<TShirt> sortedTShirts = new ArrayList<>();
+        sortedTShirts.add(tShirt5);
+        sortedTShirts.add(tShirt1);
+        sortedTShirts.add(tShirt3);
+        sortedTShirts.add(tShirt4);
+        sortedTShirts.add(tShirt2);
+        Assert.assertArrayEquals(sortedTShirts.toArray(), tShirts.toArray());
+
+    }
+
+    @Test
+    public void QuickSortTestByFabricDescTest() {
+        quickSort.sort(tShirts, byFabricDesc);
+        List<TShirt> sortedTShirts = new ArrayList<>();
+        sortedTShirts.add(tShirt2);
+        sortedTShirts.add(tShirt4);
+        sortedTShirts.add(tShirt3);
+        sortedTShirts.add(tShirt1);
+        sortedTShirts.add(tShirt5);
+        Assert.assertArrayEquals(sortedTShirts.toArray(), tShirts.toArray());
+
+    }
+}
